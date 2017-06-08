ba31418018d9e660ab39d69b549a8c3f02df7445
mattmurp <mattmurp@users.noreply.github.com>
2017-06-08 14:58:12 -0400

    Update AppScanException.java

--- src/main/java/com/ibm/appscan/plugin/core/error/AppScanException.java
+++ src/main/java/com/ibm/appscan/plugin/core/error/AppScanException.java
@@ -1,7 +1,8 @@
-/*
- * (c) Copyright IBM Corp. 2016. 
- * (c) Copyright HCL Technologies Ltd. 2017. 
-*/
+/**
+ * © Copyright IBM Corporation 2016.
+ * © Copyright HCL Technologies Ltd. 2017. 
+ * LICENSE: Apache License, Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
+ */
 
 package com.ibm.appscan.plugin.core.error;
 
